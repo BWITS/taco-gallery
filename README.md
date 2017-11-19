e933b69907f252b5f24cd8bb94f5c5b8ce1b829a
ozbillwang <ozbillwang@gmail.com>
2017-11-19 11:18:33 +0000

    update README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # taco-gallery
 A sample project for a Serverless application
+
+# its blog
+
+[Unit testing a Serverless application (part 1)](https://medium.com/a-man-with-no-server/unit-testing-a-serverless-application-b0e877142be5)
+
+[Unit testing a Serverless application (part 2)](https://medium.com/a-man-with-no-server/unit-testing-a-serverless-application-part-2-8b02f3edb9b5)
+
+[Running AWS Lambda and API Gateway locally: serverless-offline](https://medium.com/a-man-with-no-server/running-aws-lambda-and-api-gateway-locally-serverless-offline-3c64b3e54772)
--- handler.test.js
+++ handler.test.js
@@ -6,48 +6,47 @@ var TestUtils = require('./TestUtils.util');
 var assert = require('chai').assert;
 var should = require('chai').should();
 
-describe("Taco Gallery tests", function () {
-	
-	before(function(done){
-		this.timeout(50000);
-		TestUtils.mockDB().then(function(data){
-			done();
-		})
-		.catch(function(err){
-			assert(false,"Could not create the mock DB")
-			done();
-		});
-	});
+describe("Taco Gallery tests", function() {
 
-    it('the hello function should work', function(done){
-		this.timeout(50000);
-		var event={};
-		var context={};
-		var callback = (ctx, data) => {
-			done();
-		}
-		hello(event,context,callback)
-	})
-	
-	 it('should save a taco', function(done){
-		
-		var event={
-			"body":'{"name":"Al pastor","description": "Delicious taco!"}'
-		};
-		var context={};
-		var callback = (ctx, data) => {
-			try{
-				console.log(data)
-				assert(data);
-				assert(data.statusCode == 200);
-			}
-			catch (err){
-				console.log(err)
-				done(err);
-			}
-		}
-		saveTaco(event,context,callback)
-	})
+    before(function(done) {
+        this.timeout(50000);
+        TestUtils.mockDB().then(function(data) {
+                done();
+            })
+            .catch(function(err) {
+                assert(false, "Could not create the mock DB")
+                done();
+            });
+    });
 
-   
+    it('the hello function should work', function(done) {
+        this.timeout(50000);
+        var event = {};
+        var context = {};
+        var callback = (ctx, data) => {
+            done();
+        }
+        hello(event, context, callback)
+    })
+
+    it('should save a taco', function(done) {
+
+        var event = {
+            "body": '{"name":"Al pastor","description": "Delicious taco!"}'
+        };
+        var context = {};
+        var callback = (ctx, data) => {
+            try {
+                console.log(data)
+                assert(data);
+                assert(data.statusCode == 200);
+            } catch (err) {
+                console.log(err)
+                done(err);
+            }
+        }
+        saveTaco(event, context, callback)
+    })
+
+
 });
